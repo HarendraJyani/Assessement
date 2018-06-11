e0d605f8ce2cd3d3a2ae576fe5c91ed4fdee69a3
HarendraJyani <harendra.jyani@quantiphi.com>
2018-06-11 20:44:21 +0530

    Question 3 Aws with cloud watch

---
+++ StartEc2.py
@@ -0,0 +1,10 @@
+import boto3
+
+region = 'us-east-1'
+
+instances = ['i-09ef13b5c1fc57bff']
+
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2', region_name=region)
+    ec2.start_instances(InstanceIds=instances)
+    print 'started your instances: ' + str(instances)
